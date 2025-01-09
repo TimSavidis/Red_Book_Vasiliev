25c36708d38233e5d693c09bb34f0bbf388ce33a
Streltsov <123238928+TimSavidis@users.noreply.github.com>
2025-01-09 16:37:07 +0300

    3 commit

--- Chapter_01_Console/obj/Debug/net8.0/Chapter_01_Console.AssemblyInfo.cs
+++ Chapter_01_Console/obj/Debug/net8.0/Chapter_01_Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chapter_01_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49900d4f62b1dfe865e1051f28f69e6d00f9f2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chapter_01_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chapter_01_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
